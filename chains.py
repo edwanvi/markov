ea0556d8b73d1b4ed9ebb76ea7820eea77120ebb
Edwan Vi <tkdberger@gmail.com>
2016-06-20 11:52:42 -0400

    finished lab for today

--- chains.py
+++ chains.py
@@ -1,14 +1,52 @@
 import random
 import re
 
-#comments. I DID THIS AT A CLASS OKAY
 #read wonderland.txt
-
-txt_file = open("C:\Users\c-60981\Documents\wonderland.txt", 'r')
-
+txt_file = open("C:\Users\c-60981\Documents\markov\wonderland.txt", 'r')
 raw_text = txt_file.read()
 txt_file.close()
 
 #formatting
 text = re.sub('[^a-zA-Z\s]', '', raw_text).lower()
-print(text)
+words = text.split()
+
+#store occurences of a unique word after another word
+model = {}
+for i in range(len(words) - 2):
+    #word = words[i]
+    pair = words [i] + ',' + words[i + 1]
+    if pair not in model:
+        model[pair] = {}
+    node = model[pair]
+    next_word = words[i + 1]
+    if next_word not in node:
+	node[next_word] = 0
+    node[next_word] += 1
+
+# convert counts to probability
+for pair in model:
+    node = model[pair]
+    total = 0
+    for next_word in node:
+        total += node[next_word]
+    for next_word in node:
+        node[next_word] = node[next_word] / float(total)
+        
+def spinner(node):
+    spin = random.random()
+    angle = 0
+    for next_word in node:
+        angle += node[next_word]
+        if spin <= angle:
+            return next_word
+            
+def generateText():
+    pair = random.choice(model.keys()).split(',')
+    sentence = pair[0] + ' ' + pair[1]
+    length = 20
+    for i in range(length):
+        node = model[pair[0] + ',' + pair[1]]
+        word = spinner(node)
+        sentence += ' ' + word
+        pair = [pair[1], word]
+    print sentence
